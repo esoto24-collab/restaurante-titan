efadce4b25e6e1af38e8c9c037cc54dbfe39bcf9
Novato <jesusivan6413@gmail.com>
2026-08-29 16:40:12 -0700

    Persistencia de datos muy leve
    Se añadio una funcion factory para crear usurios, es como una clase
    Unos ajustes leves al formulario para que se vea un poco mejor
    Una tabla que muestra los datos guardados

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-    const main = document.querySelector("#main")
-    for(let i=0; i<9;i++){
+     for(let i=0; i<9;i++){
        const div = document.createElement("div")
        const p = document.createElement("p")
        p.textContent="Hamburgesa"
        div.appendChild(p)
        main.appendChild(div)
-    }
+     }
