5340aa9f47128c778f459adf29aa10b9dd7f84b9
Novato <jesusivan6413@gmail.com>
2026-08-31 11:13:12 -0700

    añadir carpetas
    Signed-off-by: Novato <jesusivan6413@gmail.com>
    
    Se añadio carpetas css
    Se añadio carpeta pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+const main = document.querySelector('#main');
+
 for (let i = 0; i < 8; i++) {
   const div = document.createElement('div');
+  const informativo = document.createElement('div');
+  informativo.textContent="ey ey ey"
   const p = document.createElement('p');
   p.textContent = 'Imagen';
   div.appendChild(p);
+  div.appendChild(informativo)
   main.appendChild(div);
 }
