adbbecf50deb9122836e6f815d0b9789e2c018ea
Novato <jesusivan6413@gmail.com>
2026-09-14 20:54:10 -0700

    feat: menu_hamburguesa abrir cuando se preciona
    Signed-off-by: Novato <jesusivan6413@gmail.com>

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,11 +6,16 @@ import crearMenu from './crearMenu.js';
 
 const usuarios = [];
 
+const body = document.body;
 const inicio = document.querySelector('#inicio');
 const crear_cuenta = document.querySelector('#crear_cuenta');
 const iniciar_sesion = document.querySelector('#iniciar_sesion');
 const titulo = document.querySelector('#titulo_principal');
 const menu = document.querySelector('#menu');
+const barra_lateral = document.querySelector('#barra_lateral');
+
+
+const menu_hamburguesa = document.querySelector("#menu_hamburguesa");
 
 generarInicio();
 generarRedesSociales();
@@ -20,3 +25,8 @@ iniciar_sesion.addEventListener('click', formularioLogin);
 titulo.addEventListener('click', generarInicio);
 crear_cuenta.addEventListener('click', formulario);
 menu.addEventListener('click', crearMenu);
+
+menu_hamburguesa.addEventListener("click",() =>{
+    barra_lateral.classList.toggle("visible")
+    body.classList.toggle("shadow")
+})
